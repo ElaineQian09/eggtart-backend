451712f10945548fb011c78bb8b5d71dfd4c606d
ElaineQian09 <syqian@ucla.edu>
2026-02-04 19:22:01 -0600

    fix: force railway start command

--- main.py
+++ main.py
@@ -40,6 +40,13 @@ def health_check():
     }
 
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    print("RUNNING ON PORT:", port)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
